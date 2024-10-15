fba710b8ab7b16e421966eccbc58d8b60e9c8c79
Irina <7764871+ilavva@users.noreply.github.com>
2024-10-15 18:52:08 +0300

    img alt

--- src/Components/Dish/Dish.tsx
+++ src/Components/Dish/Dish.tsx
@@ -3,7 +3,7 @@ import { DishInfo } from "../FoodDishes/FoodDishes";
 export const Dish = (props: { theDish: DishInfo }) => {
   return (
     <div className="dish-card">
-      <img src={props.theDish.strMealThumb} />
+      <img src={props.theDish.strMealThumb} alt={props.theDish.strMeal} />
       <div>
         <p key={props.theDish.idMeal}>{props.theDish.strMeal}</p>
         <p>{props.theDish.idMeal}</p>
