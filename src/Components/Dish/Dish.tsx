39160ce14267ca1f86d29bc59b9cebf050a71aba
Irina <7764871+ilavva@users.noreply.github.com>
2024-10-15 15:34:12 +0300

    fix for compilation

--- src/Components/Dish/Dish.tsx
+++ src/Components/Dish/Dish.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import "./Dish.css";
 import { DishInfo } from "../FoodDishes/FoodDishes";
 export const Dish = (props: { theDish: DishInfo }) => {
   return (
     <div className="dish-card">
-      <img src={`${props.theDish.strMealThumb}/preview`} />{" "}
+      <img src={props.theDish.strMealThumb} />
       <div>
         <p key={props.theDish.idMeal}>{props.theDish.strMeal}</p>
         <p>{props.theDish.idMeal}</p>
