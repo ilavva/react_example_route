d8ef9a7bccb11e3ba4f101bee1b83301e73ef741
Irina <7764871+ilavva@users.noreply.github.com>
2024-10-15 20:56:13 +0300

    lint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import "./App.css";
-// import { Food } from "./Components/Food/Food";
+
 import MainScreen from "./Components/MainScreen/MainScreen";
-import { Conference } from "./Components/Conference/Conference";
-// import { Food } from "./Components/Food/Food";
-// import { Gallery } from "./Components/Gallery/Gallery";
-// import MainScreen from "./Components/MainScreen/MainScreen";
-// import { Parent1 } from "./Components/Parent1/Parent1";
 
 function App() {
   return (
     <div>
       <MainScreen></MainScreen>
-      {/* <Parent1></Parent1> */}
-      {/* <Food></Food> */}
-      {/* <Food></Food> */}
-      <Conference />
     </div>
   );
 }
--- src/Components/NewNavBar/NavBarInfo.ts
+++ src/Components/NewNavBar/NavBarInfo.ts
@@ -7,7 +7,7 @@ import { Food } from "../Food/Food";
 
 import { Lesson1 } from "../Lesson1/Lesson1";
 import { Parent1 } from "../Parent1/Parent1";
-
+import { Conference } from "../Conference/Conference";
 export type NavItem = {
   key: number;
   displayStr: string;
@@ -30,36 +30,42 @@ export const arrForNav = [
   },
   {
     key: 2,
+    displayStr: "Conference",
+    hrefStr: "/conference",
+    refComponent: Conference,
+  },
+  {
+    key: 3,
     displayStr: "Products",
     hrefStr: "/products",
     refComponent: Lesson1,
   },
   {
-    key: 3,
+    key: 4,
     displayStr: "Meals",
     hrefStr: "/food",
     refComponent: Food,
   },
   {
-    key: 4,
+    key: 5,
     displayStr: "Contact Us",
     hrefStr: "/contact",
     refComponent: ContactUs,
   },
   {
-    key: 5,
+    key: 6,
     displayStr: "demo-01",
     hrefStr: "/demo-01",
     refComponent: Parent1,
   },
   {
-    key: 6,
+    key: 7,
     displayStr: "Gallery",
     hrefStr: "/gallery",
     refComponent: Gallery,
   },
   {
-    key: 7,
+    key: 8,
     displayStr: "Recipe",
     hrefStr: "/cheese",
     refComponent: CheesecakeRecipe,
