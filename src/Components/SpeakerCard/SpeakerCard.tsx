572a5c2a67618c7e577396b034ad672086efab27
Irina <7764871+ilavva@users.noreply.github.com>
2024-10-15 20:56:26 +0300

    lint rules

---
+++ src/Components/SpeakerCard/SpeakerCard.tsx
@@ -0,0 +1,51 @@
+import "./SpeakerCard.css";
+
+import React from "react";
+export type Speaker = {
+  login: {
+    uuid: string;
+  };
+  gender: string;
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  email: string;
+  picture: {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  };
+  dob: {
+    age: number;
+  };
+  phone: string;
+  cell: string;
+  nat: string;
+  location: {
+    city: string;
+    state: string;
+    country: string;
+
+    coordinates: {
+      latitude: string;
+      longitude: string;
+    };
+    timezone: {
+      offset: string;
+      description: string;
+    };
+  };
+};
+export const SpeakerCard = (props: { speaker: Speaker }) => {
+  return (
+    <div className="SpeakerCard">
+      <img src={props.speaker.picture.thumbnail} alt="" />
+      <p>
+        {" "}
+        {props.speaker.name.first} {props.speaker.name.last}
+      </p>
+    </div>
+  );
+};
---
+++ src/Components/SpeakersGallery/SpeakersGallery.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import "./SpeakersGallery.css";
+import { Speaker, SpeakerCard } from "../SpeakerCard/SpeakerCard";
+
+//https://randomuser.me/api/?results=50
+export const SpeakersGallery = (props: {
+  setWhichSpeaker: React.Dispatch<React.SetStateAction<Speaker | undefined>>;
+}) => {
+  const [speackersArr, setSpeakersArr] = useState<Speaker[]>([]);
+  useEffect(() => {
+    fetch(`https://randomuser.me/api/?results=50`)
+      .then((dataFromAPI) => {
+        return dataFromAPI.json();
+      })
+      .then((dataAsObj) => setSpeakersArr(dataAsObj.results as Speaker[]));
+  }, []);
+
+  const aSpeakerWasClicked = (theSpkr: Speaker) => {
+    props.setWhichSpeaker(theSpkr);
+  };
+
+  return (
+    <div>
+      <h2>Speakers Gallery</h2>
+      <div className="SpeakersGallery">
+        {speackersArr &&
+          speackersArr.map((curr) => {
+            return (
+              <div
+                key={curr.login.uuid}
+                onClick={(e) => {
+                  return aSpeakerWasClicked(curr);
+                }}
+                onKeyDown={(e) => {
+                  // Trigger the click handler when the Enter key is pressed
+                  if (e.key === "Enter" || e.key === " ") {
+                    aSpeakerWasClicked(curr);
+                  }
+                }}
+                tabIndex={0} // Makes the div focusable
+                role="button" // Optional: improves accessibility by indicating that it's a button
+                style={{ cursor: "pointer" }} // Optional: indicates that the element is clickable
+              >
+                <SpeakerCard speaker={curr} />
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
